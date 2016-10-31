0fd3ff1060156cd9dd5890cf8efe0e8f98b76ecc
mac <mac@macdemacbook-pro.local>
2016-10-31 09:22:04 +0800

    Initial Commit

---
+++ 021-- 图片轮播（无限）/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  021-- 图片轮播（无限）
+//
+//  Created by mac on 16/10/31.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 021-- 图片轮播（无限）UITests/_21___________UITests.m
@@ -0,0 +1,40 @@
+//
+//  _21___________UITests.m
+//  021-- 图片轮播（无限）UITests
+//
+//  Created by mac on 16/10/31.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _21___________UITests : XCTestCase
+
+@end
+
+@implementation _21___________UITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
